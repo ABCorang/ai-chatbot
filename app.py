553774e9534b06960547e9584dc76983f599a57f
user <172138601+ABCorang@users.noreply.github.com>
2024-07-03 01:45:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ import os
 # Gemini APIの設定
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
-model_name = st.sidebar.radio("モデルを選択してください",
-                    ["gemini-1.5-flash-latest",
-                    "gemini-1.5-pro-latest", 
-                    "gemini-1.5-flash-001"])
+model_name = st.sidebar.radio('モデルを選択してください',
+                    ['gemini-1.5-pro-latest', 
+                    'gemini-1.5-flash-latest',
+                    'gemini-1.5-flash-001'])
 
 # 温度設定
-temperature = st.sidebar.slider("temperature", 
+temperature = st.sidebar.slider('temperature', 
                   min_value=0.0, 
                   max_value=1.0, 
                   value=0.5)
 # 生成設定
 generation_config = {
-    "temperature": temperature
+    'temperature': temperature
 }
 
 # モデルのインスタンス化
@@ -46,19 +46,19 @@ if user_input:
     # ユーザーメッセージを追加
     st.session_state.chat_history.append({'role': 'user', 'content': user_input})
         
-    with st.spinner("LLM is typing ..."):
+    with st.spinner('LLM is typing ...'):
         # 関数からモデルを取得
         model = get_model(model_name, generation_config)
         # AIレスポンスを生成
         ai_response = model.generate_content(user_input)
-        
+
     # AIレスポンスを追加
     st.session_state.chat_history.append({'role': 'assistant', 'content': ai_response.text})
     # チャット履歴を更新
     st.rerun()
 
 # チャット履歴クリアボタン
-clear_btn = st.sidebar.button("チャット履歴をクリア")
+clear_btn = st.sidebar.button('チャット履歴をクリア')
 if clear_btn:
     st.session_state.chat_history = []
     st.rerun()
