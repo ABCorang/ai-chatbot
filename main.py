bdde9e833755f7fd6898ec7ef9ddcc4f3d0314f2
user <172138601+ABCorang@users.noreply.github.com>
2024-07-02 22:39:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,37 @@ import os
 
 # Gemini APIの設定
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-model = genai.GenerativeModel(model_name='gemini-1.5-flash-latest')
 
-# Streamlitアプリケーションの基本構造
+model_name = st.sidebar.radio("モデルを選択してください",
+                    ["gemini-1.5-flash-latest",
+                    "gemini-1.5-pro-latest", 
+                    "gemini-1.5-flash-001"])
+
+# 温度設定
+temperature = st.sidebar.slider("temperature", 
+                  min_value=0.0, 
+                  max_value=1.0, 
+                  value=0.5)
+# 生成設定
+generation_config = {
+    "temperature": temperature
+}
+
+# モデルのインスタンス化
+model = genai.GenerativeModel(model_name, generation_config=generation_config)
+
+
 st.title('AIチャットアシスタント')
 
 # チャット履歴の初期化
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
 
 
 
 
 # チャット履歴の表示
-for message in st.session_state.messages:
+for message in st.session_state.chat_history:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
@@ -26,17 +43,22 @@ user_input = st.chat_input('メッセージを入力してください:')
 # 送信ボタン
 if user_input:
     # ユーザーメッセージを追加
-    st.session_state.messages.append({'role': 'user', 'content': user_input})
+    st.session_state.chat_history.append({'role': 'user', 'content': user_input})
         
     with st.spinner("LLM is typing ..."):
 
         # AIレスポンスを生成
-        model_response = model.generate_content(user_input)
+        ai_response = model.generate_content(user_input)
     
-        # AIレスポンスを追加
-    st.session_state.messages.append({'role': 'assistant', 'content': model_response.text})
-    st.session_state.user_input = "" # テキストボックスをクリア
-
-
+    # AIレスポンスを追加
+    st.session_state.chat_history.append({'role': 'assistant', 'content': ai_response.text})
     # チャット履歴を更新
     st.rerun()
+
+# チャット履歴クリアボタン
+clear_btn = st.sidebar.button("チャット履歴をクリア")
+if clear_btn:
+    st.session_state.chat_history = []
+    st.rerun()
+
+
